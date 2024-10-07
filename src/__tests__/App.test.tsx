7d1685546257b5d65831d5c386c4d4cc2a5ae341
Maxim <murmax82@gmail.com>
2024-10-07 18:54:21 +0400

    mock electron in tests

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,6 +2,18 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import App from '../renderer/App';
 
+const mockElectron = {
+  ipcRenderer: {
+    openSelectDirectoryDialog: jest.fn(),
+    processDirectory: jest.fn(),
+    onDirectoryResult: jest.fn(),
+  },
+};
+
+beforeAll(() => {
+  global.window.electron = mockElectron;
+});
+
 describe('App', () => {
   it('should render', () => {
     expect(render(<App />)).toBeTruthy();
